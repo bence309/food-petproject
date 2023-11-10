5b76ce8f518c0902bf0da8d514e7321eb7c885ec
Bence Bodi <bence309@hotmail.com>
2023-11-10 14:45:15 +0100

    started working on backend routing

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+// Inside App.js
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Burgers from './Burgers'; // Adjust the path
+import SideDishes from './SideDishes'; // Adjust the path
+import Drinks from './Drinks'; // Adjust the path
+import Support from './Support'; // Adjust the path
+
+const App = () => {
+  return (
+    <div>
+      <Route path="/burgers" component={Burgers} />
+      <Route path="/side-dishes" component={SideDishes} />
+      <Route path="/drinks" component={Drinks} />
+      <Route path="/support" component={Support} />
+    </div>
+  );
+};
+
+export default App;
+
