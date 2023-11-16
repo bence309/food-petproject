8f0feff52b18dccf52ad14b0b79332eeee112282
Bence Bodi <bence309@hotmail.com>
2023-11-16 07:59:07 +0100

    fixed App.js paths, made the buttons bigger

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Burgers from './Burgers'; // Adjust the path
-import SideDishes from './SideDishes'; // Adjust the path
-import Drinks from './Drinks'; // Adjust the path
-import Support from './Support'; // Adjust the path
+import Burgers from './Components/Burgers'; // Adjust the path
+import Sidedishes from './Components/Sidedishes'; // Adjust the path
+import Drinks from './Components/Drinks'; // Adjust the path
+import Support from './Components/Support'; // Adjust the path
 
 const App = () => {
   return (
     <div>
       <Route path="/burgers" component={Burgers} />
-      <Route path="/side-dishes" component={SideDishes} />
+      <Route path="/side-dishes" component={Sidedishes} />
       <Route path="/drinks" component={Drinks} />
       <Route path="/support" component={Support} />
     </div>
